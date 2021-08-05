69555863562c29503bd5d56c7c40ea37ab441e33
Unbuttun <unbuttun@gmail.com>
2021-08-05 22:44:24 +0530

    fix(main): 🐛 Fixing various small bugs here and there [skip ci]

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,16 @@
 import type { Options } from "tsup";
 
+const env = process.env.NODE_ENV;
+
 export const tsup: Options = {
   splitting: true,
-  sourcemap: true,
+  sourcemap: env === "production",
   clean: true,
   dts: true,
   format: ["cjs", "esm"],
-  minify: true,
+  minify: env === "production",
   bundle: true,
+  watch: env === "development",
   skipNodeModulesBundle: true,
   entryPoints: ["src/index.ts"],
 };
